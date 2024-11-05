3a1ec75dbd2753cad52e063a89c32013755e8787
RoknuzzamanRokon <rokon.raz@gmail.com>
2024-11-06 03:20:08 +0600

    Add function for get and update Country Code.

--- single_hotel_info_input_data_in_db_json_formet.py
+++ single_hotel_info_input_data_in_db_json_formet.py
@@ -33,6 +33,43 @@ def only_column_info(table, column, engine):
         return []
 
 
+def only_select_column_info(table, column, country_code, engine):
+    """Fetch distinct values from a specified column in a given table."""
+    try:
+        query = f"SELECT {column} FROM {table} WHERE StatusUpdateHotelInfo != 'Done Json' OR StatusUpdateHotelInfo IS NULL AND CountryCode = '{country_code}';"
+        df = pd.read_sql(query, engine)
+        
+
+        unique_values = df[column].unique()
+        # print(len(uniqe_value))
+        
+        # len_list = len(list)
+        # print(len_list)
+
+        all_values = df[column].tolist()
+        
+        # Count unique values
+        unique_values = df[column].unique()
+        # print(f"Number of unique values: {len(unique_values)}")
+        
+        # # Find duplicates
+        # duplicates = df[df[column].duplicated()][column].tolist()  
+        # duplicate_counts = df[column].value_counts()[df[column].value_counts() > 1]  
+
+        # print(f"Total values: {len(all_values)}")
+        # print(f"Duplicate values: {duplicates}")
+        # print(f"Duplicate counts:\n{duplicate_counts}")
+
+        return unique_values
+    except Exception as e:
+        print(f"Error fetching column info: {e}")
+        return [], pd.Series()
+        
+data = only_select_column_info('hotel_info_all', 'SystemId', 'AE', engine)
+
+
+
+
 def fetch_hotel_info_by_systemId(systemId):
     """Fetch hotel information by system ID using synchronous requests."""
     url = "https://api.giinfotech.ae/api/Hotel/HotelInfo"
@@ -96,8 +133,7 @@ def main():
     formatted_start_time = datetime.fromtimestamp(start_time).strftime("%I:%M %p")  
     print(f"Start Time: {formatted_start_time}")
 
-    table = 'hotel_info_all'
-    system_ids = only_column_info(table=table, column='SystemId', engine=engine)
+    system_ids = only_select_column_info('hotel_info_all', 'SystemId', 'AE', engine)
 
     for index, systemId in enumerate(system_ids, start=1):
         hotel_info = fetch_hotel_info_by_systemId(systemId)
